3d2828bff0d436c30bb1c5cfa79cdc92520efcda
2w1 113090 farias desaa nicolas santiago <113090@tecnicatura.frc.utn.edu.ar>
2022-08-21 02:12:37 -0300

    chapter 2
    chapter 2 almost complete, missing the exercises from the final part

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,77 @@
+﻿using static System.Console;
+namespace Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"There are {args.Length} arguments.");
+
+            foreach(string arg in args)
+            {
+                WriteLine(arg);
+            }
+
+            if (args.Length<3)
+            {
+                WriteLine("You must specify two colors and a cursors size, e.g.");
+                WriteLine("dotnet run red yellow 50");
+                return; //stop running
+            }
+
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true);
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true);
+
+
+            try
+            { 
+                CursorSize = int.Parse(args[2]);
+            
+            }
+
+            catch (PlatformNotSupportedException)
+            {
+                WriteLine("The current platform doest not support changing the size of the cursor.");
+            }
+
+            //Another way to handle differences in operating systems is to use the OperatingSystem class in
+            //the System namespace, as shown in the following code:
+
+            if (OperatingSystem.IsWindows())
+            {
+                // execute code that only works on Windows
+            }
+            else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+            {
+                // execute code that only works on Windows 10 or later
+            }
+            else if (OperatingSystem.IsIOSVersionAtLeast(major: 14, minor: 5))
+            {
+                // execute code that only works on iOS 14.5 or later
+            }
+            else if (OperatingSystem.IsBrowser())
+            {
+                // execute code that only works in the browser with Blazor
+            }
+
+            //You can then write statements that will compile only for the specified platforms, as shown in
+            //the following code:
+
+            #if NET6_0_ANDROID
+            //compile statements that only works on Android
+            #elif NET_6_0_IOS
+            //compile statments that only works on iOS
+            #else
+            //compile statements that work everywhere else
+            #endif
+
+        }
+    }
+}
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Xml;
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88; //storing as double in an object
+            object name = "Amir";
+            Console.WriteLine($"{name} is {height} metres tall.");
+
+            //int length = name.Length; //gives compile error!
+            int length2 = ((string)name).Length;
+            Console.WriteLine($"{name} has {length2} characters.");
+
+            //storing a string in a dynamic object
+            //string has a Length property
+            dynamic something = "Ahmed";
+
+            //int does not have a Length property
+            something = 12;
+
+            // an array of any type has a Length property
+            something = new[] { 3, 5, 7 };
+
+            // this compiles but would throw an exception at run-tume
+            // if you later store data type that does not have a 
+            // property named Length
+            Console.WriteLine($"Length is {something.Length}");
+
+            var population = 66_000_000; //66 million in UK
+            var weight = 1.88; // in kilograms
+            var price = 4.99M; //in pounds sterling
+            var fruit = "Apples"; //string use double-quotes
+            var letter = 'Z'; //chars use single-quotes
+            var happy = true; //Booleans have value of true or false
+
+            //good use of var because it avoids the repeated type
+            //as shown in the more verbose second statement
+            var xml1 = new XmlDocument();
+            XmlDocument xml2 = new XmlDocument();
+
+            //bad use of the var because we cannot thel the type, so we
+            //should use a specific tpye declaration as shown in
+            //the second statement
+            var file1 = File.CreateText("something1.text");
+            StreamWriter file2 = File.CreateText("something2.text");
+
+            XmlDocument xml3 = new();
+
+
+            Console.WriteLine($"default(int) = {default(int)}");
+            Console.WriteLine($"default(bool) = {default(bool)}");
+            Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default(string) = {default(string)}");
+
+            int number = 13;
+            Console.WriteLine($"number has been set to: {number}");
+            number = default;
+            Console.WriteLine($"number has been reset to its default: {number}");
+
+            string[] names;
+            names = new string[4];
+
+            //storing items at index positions
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+
+            for(int i = 0; i< names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            string[] names2 = new[] { "Kate", "Jack", "Rebecca", "Tom" };
+
+
+        }
+    }
+}
+
+
+
